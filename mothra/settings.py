a28bf17a4af2c58ba570bdb5d7e025c1f23ad727
BowWowWow <jburns@dataium.com>
2014-10-17 08:28:26 -0500

    modified for local mysql

--- mothra/settings.py
+++ mothra/settings.py
@@ -101,8 +101,16 @@ WSGI_APPLICATION = 'mothra.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       # 'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+	'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'mothra',
+	'USER': 'root',
+	'PASSWORD': 'Voyager26!',
+	'HOST': 'localhost',
+	'PORT': '3306',
+        'STORAGE_ENGINE':'MyISAM',
     }
 }
 
