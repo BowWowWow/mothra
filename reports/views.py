c255d68e29f9813dacbc7a5872b17a635cfe511e
BowWowWow <jburns@dataium.com>
2014-11-12 13:47:27 -0600

    update views for dealer groupj

--- reports/views.py
+++ reports/views.py
@@ -70,10 +70,13 @@ def user_dhl(request):
         dealeruser = User.objects.get(id=user_id,userprofile__wants_dailyhitlist=True,userprofile__dealergroup__dealergroupinactive='N',userprofile__dealergroup__dealergroupdeleted='N')
         print 'this user is a member of a group!'
         dealergroup = dealeruser.userprofile.dealergroup
-        # print dealergroup
+        print dealergroup
         dhldealer = Dealer.objects.filter(dealergroup=dealeruser.userprofile.dealergroup)
-        dhlleads = DealerDailyHitList.objects.filter(dealersite=dhldealer).order_by('-shopper_intensity')
-        print locals()
+        print dhldealer
+        dhldealersite = DealerSite.objects.filter(dealer=dhldealer)
+        print dhldealersite 
+        dhlleads = DealerDailyHitList.objects.filter(dealersite=dhldealersite).order_by('-shopper_intensity')
+        # print locals()
       
 
     return render_to_response('userdhl.html',locals(),context_instance=RequestContext(request))  
